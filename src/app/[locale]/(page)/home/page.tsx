1dc93a45c258330f5477032dbe9254e68033f773
DuongTrung2212 <duongtrungqb12@gmail.com>
2023-09-28 15:52:04 +0700

    update card item

--- src/app/[locale]/(page)/home/page.tsx
+++ src/app/[locale]/(page)/home/page.tsx
@@ -1,7 +1,7 @@
 'use client';
 import { Input, Segmented, Select } from 'antd';
 import React from 'react';
-import { useTheme } from 'next-themes'
+import { useTheme } from 'next-themes';
 import ProductItem from '@/components/Contents/Home/ProductItem';
 
 const { Search } = Input;
@@ -16,7 +16,7 @@ export default function HomePage() {
           style={{ width: 500 }}
         />
       </div>
-      <div className='w-1/2 m-auto flex justify-around mt-[50px]'>
+      <div className="w-1/2 m-auto flex items-center justify-around mt-[50px]">
         <Select
           labelInValue
           defaultValue={{ value: 'popular', label: 'Popular' }}
@@ -33,9 +33,27 @@ export default function HomePage() {
             },
           ]}
         />
-        <Segmented size={'large'} options={['All', 'Fammer', 'Seed Company', 'Distributer', 'Factory']} />
+        <Segmented
+          size={'large'}
+          options={['All', 'Fammer', 'Seed Company', 'Distributer', 'Factory']}
+        />
       </div>
-        <ProductItem/>
+      <div className="w-4/5 flex flex-wrap gap-y-24 gap-x-12 m-auto mt-[80px] item-center justify-center">
+        {[...Array(5)].map((_, index) => (
+          <ProductItem
+            key={index}
+            productId="1"
+            productName="Sầu riêng DatBe"
+            productImg="https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png"
+            ownerName="SimpRaidenEi"
+            ownerImg="https://xsgames.co/randomusers/avatar.php?g=pixel"
+            role="Fammer"
+            likeQuantity={12}
+            messageQuantity={12}
+            buyerQuantity={12}
+          />
+        ))}
+      </div>
     </div>
   );
 }
--- src/components/Contents/Home/ProductItem/index.tsx
+++ src/components/Contents/Home/ProductItem/index.tsx
@@ -1,36 +1,79 @@
 import {
   EditOutlined,
   EllipsisOutlined,
+  HeartOutlined,
+  LikeOutlined,
+  MessageOutlined,
   SettingOutlined,
+  ShoppingCartOutlined,
 } from '@ant-design/icons';
-import { Avatar, Card, Image } from 'antd';
+import { Avatar, Card, Image, Space, Statistic, Tag, Typography } from 'antd';
 import React from 'react';
 
 const { Meta } = Card;
 
-export default function ProductItem() {
+interface Props {
+  productName: string;
+  productImg: string;
+  productId: string;
+  ownerName: string;
+  ownerImg: string;
+  role: string;
+  likeQuantity: number;
+  messageQuantity: number;
+  buyerQuantity: number;
+}
+
+export default function ProductItem(props: Props) {
   return (
-    <div>
+    <div className="w-fit">
       <Card
         style={{ width: 300 }}
         cover={
-          <Image
-            alt="example"
-            src="https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png"
-          />
+          <div className="relative w-fit">
+            <Image
+              width={300}
+              height={200}
+              alt=""
+              className="object-cover"
+              src={props.productImg}
+            />
+          </div>
         }
         actions={[
-          <SettingOutlined key="setting" />,
-          <EditOutlined key="edit" />,
-          <EllipsisOutlined key="ellipsis" />,
+          <Statistic
+            key="like"
+            valueStyle={{ fontSize: '10px' }}
+            title={<LikeOutlined />}
+            value={`112893`}
+          />,
+          <Statistic
+            key="message"
+            valueStyle={{ fontSize: '10px' }}
+            title={<MessageOutlined />}
+            value={`${props.likeQuantity} Messenger`}
+          />,
+          <Statistic
+            key="cart"
+            valueStyle={{ fontSize: '10px' }}
+            title={<ShoppingCartOutlined />}
+            value={`${props.buyerQuantity} Buyer`}
+          />,
         ]}
       >
         <Meta
-          avatar={
-            <Avatar src="https://xsgames.co/randomusers/avatar.php?g=pixel" />
+          avatar={<Avatar src={props.ownerImg} />}
+          title={
+            <div>
+              <Meta title={props.productName} />
+            </div>
           }
-          title="Card title"
-          description="This is the description"
+          description={
+            <Space direction={'vertical'}>
+              <Typography.Text italic>{props.ownerName}</Typography.Text>
+              <Tag>{props.role}</Tag>
+            </Space>
+          }
         />
       </Card>
     </div>
