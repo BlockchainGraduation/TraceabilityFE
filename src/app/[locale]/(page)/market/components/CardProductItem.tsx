be2031b1ea6e0fd399845ddbf578bf40c74a6239
DuongTrung2212 <duongtrungqb12@gmail.com>
2023-11-06 12:16:08 +0700

    add page  market

---
+++ src/app/[locale]/(page)/market/components/CardProductItem.tsx
@@ -0,0 +1,58 @@
+import currency from '@/services/currency';
+import staticVariables from '@/static';
+import {
+  faBox,
+  faCoins,
+  faFileSignature,
+  faUserTie,
+} from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { Badge, Image, Space, Tag } from 'antd';
+import React from 'react';
+
+export default function CardProductItem(props: MarketType) {
+  return (
+    <Badge.Ribbon text="Hippies">
+      <div
+        style={{ boxShadow: `0 2px 8px rgba(0,0,0,.15)` }}
+        className="flex p-[5px] rounded-xl "
+      >
+        <div
+          className="w-1/3 rounded-xl overflow-hidden"
+          style={{ boxShadow: `0 2px 8px rgba(0,0,0,.15)` }}
+        >
+          <Image
+            className=" object-cover"
+            preview={false}
+            width={'100%'}
+            height={'100%'}
+            alt=""
+            src={props.product?.banner}
+          />
+        </div>
+        <div className="flex flex-col w-2/3 space-y-1 text-[16px] px-[50px] py-[20px]">
+          <p className="text-[20px] font-bold">{props.product?.name}</p>
+          <Space>
+            <FontAwesomeIcon icon={faBox} style={{ color: '#30b046' }} />
+            <p>{props.product?.quantity}</p>
+          </Space>
+          <Space>
+            <FontAwesomeIcon
+              icon={faFileSignature}
+              style={{ color: '#2c963d' }}
+            />
+            <Tag color={'success'}>{props.product?.product_type}</Tag>
+          </Space>
+          <Space>
+            <FontAwesomeIcon icon={faCoins} style={{ color: '#3da451' }} />
+            <p>{`${props.product?.price} ${currency}`}</p>
+          </Space>
+          <Space>
+            <FontAwesomeIcon icon={faUserTie} style={{ color: '#295094' }} />
+            <p>Sản phẩm của {props.product?.user?.username}</p>
+          </Space>
+        </div>
+      </div>
+    </Badge.Ribbon>
+  );
+}
--- src/app/[locale]/(page)/market/page.tsx
+++ src/app/[locale]/(page)/market/page.tsx
@@ -1,10 +1,105 @@
-import { Steps } from 'antd';
-import React from 'react';
+'use client';
+import staticVariables from '@/static';
+import {
+  Collapse,
+  CollapseProps,
+  Image,
+  List,
+  Pagination,
+  Radio,
+  RadioChangeEvent,
+} from 'antd';
+import React, { useCallback, useEffect, useState } from 'react';
+import instanceAxios from '@/api/instanceAxios';
+import CardProductItem from './components/CardProductItem';
 
-export default function Market() {
+export default function MarketPage() {
+  const [valueRadio, setValueRadio] = useState('FARMER');
+  const [loading, setLoading] = useState(true);
+  const [totalMarket, setTotalMarket] = useState(0);
+  const [listMarket, setListMarket] = useState<MarketType[]>([]);
+  const [limit, setLimit] = useState(10);
+  const [currentPage, setCurrentPage] = useState(1);
+
+  const fethListProduct = useCallback(async () => {
+    await instanceAxios
+      .get(
+        `marketplace/list?order_type=${valueRadio}&skip=${
+          currentPage - 1
+        }&limit=${limit}`
+      )
+      .then((res) => {
+        setListMarket(res.data.data.list_marketplace);
+        setTotalMarket(res.data.data.total_marketplace);
+      })
+      .catch((err) => console.log(err));
+  }, [currentPage, limit, valueRadio]);
+  useEffect(() => {
+    fethListProduct();
+  }, [fethListProduct]);
+  const onChangeRadio = (e: RadioChangeEvent) => {
+    console.log('radio checked', e.target.value);
+    setValueRadio(e.target.value);
+  };
+  const items: CollapseProps['items'] = [
+    {
+      key: '1',
+      label: 'Loại sản phẩm',
+      children: (
+        <Radio.Group
+          className="flex flex-col space-y-2"
+          onChange={onChangeRadio}
+          value={valueRadio}
+        >
+          <Radio value={'FARMER'}>Fammer</Radio>
+          <Radio value={'SEEDLING_COMPANY'}>Cây giống</Radio>
+          <Radio value={'MANUFACTURER'}>Sản phẩm nhà máy</Radio>
+        </Radio.Group>
+      ),
+      // extra: <p>{`>`}</p>,
+    },
+  ];
+  const onChange = (key: string | string[]) => {
+    console.log(key);
+  };
   return (
-    <div>
-      <div>Hienj tai ban san pham nao</div>
+    <div className="flex w-4/5  m-auto">
+      <div className="w-1/5">
+        <Image
+          className="object-cover rounded-2xl"
+          width={'100%'}
+          height={150}
+          src={staticVariables.qc5.src}
+          alt=""
+        />
+        <div
+          style={{ boxShadow: `0 2px 8px rgba(0,0,0,.15)` }}
+          className="w-full shadow-lg rounded-xl overflow-hidden mt-5"
+        >
+          <Collapse
+            bordered={false}
+            items={items}
+            defaultActiveKey={['1']}
+            onChange={onChange}
+          />
+        </div>
+      </div>
+      <div className="w-4/5 px-[50px]">
+        <p className="text-[18px] pb-[20px]">
+          <strong>{totalMarket}</strong> sản phẩm phù hợp với tìm kiếm của bạn
+        </p>
+        <div className="flex flex-col gap-y-5">
+          {listMarket.map((item, index) => (
+            <CardProductItem key={index} {...item} />
+          ))}
+        </div>
+        <Pagination
+          className="mx-auto block w-fit mt-5"
+          current={currentPage}
+          total={totalMarket}
+          onChange={(e) => setCurrentPage(e)}
+        />
+      </div>
     </div>
   );
 }
